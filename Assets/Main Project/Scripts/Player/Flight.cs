cf474ff0a5093810a6735dd1f9eea35ed37609eb
HarrisonBakerSussex <hjmb20@sussex.ac.uk>
2025-01-22 17:56:37 +0000

    Fixed Uncalled Function

--- Assets/Main Project/Scripts/Player/Flight.cs
+++ Assets/Main Project/Scripts/Player/Flight.cs
@@ -49,7 +49,7 @@ public class Flight : MonoBehaviour
     private void FixedUpdate(){
         CalculateGravity();
         CalculateThrust();
-        CalculateThrust();
+        CalculateTorques();
         EnableTrails();
     }
 
